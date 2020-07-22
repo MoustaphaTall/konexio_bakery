2e592ffa6fe0bb8157af34cfebadecd694d6afcd
MoustaphaTall <65893882+MoustaphaTall@users.noreply.github.com>
2020-07-22 18:09:20 +0200

    correct activeTab switch

--- src/App.js
+++ src/App.js
@@ -6,72 +6,59 @@ import Button from './components/core/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'rc-slider/assets/index.css';
 
-const add = <Add />
-const list = <List />
-const pay = <Pay />
-
 class App extends Component {
   constructor(props) {
     super(props);
     
     this.state = {
-      activeTab: add,
-      items: [],
-      areButtonsSelected: {
-        first: true,
-        second: false,
-        third: false
-      }                  
+      activeTab: "add",
+      items: [],         
     }
 
     this.onClickTabAdd = this.onClickTabAdd.bind(this);
     this.onClickTabList = this.onClickTabList.bind(this);
     this.onClickTabPay = this.onClickTabPay.bind(this);
+    this.onAdd = this.onAdd.bind(this);
   }
 
   onAdd(price, input) {
+
   }
 
   onClickTabAdd() {        
     this.setState({
-      activeTab: add,
-      areButtonsSelected : {
-        first: true
-      } 
+      activeTab: "add",      
     });       
   }
 
   onClickTabList() {
     this.setState({
-      activeTab: list,
-      areButtonsSelected : {
-        second: true
-      } 
+      activeTab: "list",
     });        
   }
 
   onClickTabPay() {
     this.setState({
-      activeTab: pay,
-      areButtonsSelected : {
-        third: true
-      } 
+      activeTab: "pay",      
     });    
   }
 
-  render() {    
+  render() {
+    const activeTab = this.state.activeTab;    
     return (
       <div className="container">
         <div className="row">
           <div className="col-4">
-            <Button isSelected={this.state.areButtonsSelected.first} onClick={this.onClickTabAdd}>Add</Button>
-            <Button isSelected={this.state.areButtonsSelected.second} onClick={this.onClickTabList}>List</Button>
-            <Button isSelected={this.state.areButtonsSelected.third} onClick={this.onClickTabPay}>Pay</Button>            
+            <Button isSelected={activeTab === "add"} onClick={this.onClickTabAdd}>Add</Button>
+            <Button isSelected={activeTab === "list"} onClick={this.onClickTabList}>List</Button>
+            <Button isSelected={activeTab === "pay"} onClick={this.onClickTabPay}>Pay</Button>            
           </div>
         </div>
         <div className="row">
           <div className="col-8">
-            {this.state.activeTab}
+            {activeTab === "add" ? <Add onAdd={this.onAdd()} /> : null}
+            {activeTab === "list" ? <List /> : null}
+            {activeTab === "pay" ? <Pay /> : null}
           </div>
         </div>        
       </div>
